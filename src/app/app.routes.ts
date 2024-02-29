32504ba7c3834c761852f173dd79447e62f0b31e
Ebrahim Abd EL Razik <ebrahimabdelrazik2002@gmail.com>
2024-02-29 18:08:26 +0200

    Weather App with Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,9 @@
 import { Routes } from '@angular/router';
+import { WeatherListComponent } from './Components/weather-list/weather-list.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-
-    redirectTo: 'weather-list',
-    pathMatch: 'full',
-  },
-  {
-    path: 'weather-list',
-    loadComponent: () =>
-      import('./Components/weather-list/weather-list.component').then(
-        (m) => m.WeatherListComponent
-      ),
-  },
-
+  { path: '', redirectTo: 'weather-list', pathMatch: 'full' },
+  { path: 'weather-list', component: WeatherListComponent },
   {
     path: 'weather/:city',
     loadComponent: () =>
